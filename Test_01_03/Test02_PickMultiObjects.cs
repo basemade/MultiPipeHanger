26e070b9b15bd233a98dc718d2d64a37cf580e33
basemade.mp <basemade.mp@gmail.com>
2022-11-03 18:04:56 +0800

    加入專案檔案。

---
+++ Test_01_03/Test02_PickMultiObjects.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+
+namespace Test_01_03
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+
+
+    #region(執行外掛後點選取多個物件的方法_1)
+    /*
+    public class Test02_PickMultiObjects : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            Selection sel = uidoc.Selection;
+            StringBuilder st = new StringBuilder();
+
+            
+
+            //IList<Element> elems = sel.PickObjects(ObjectType.Element, "選擇牆");
+            //上面這行報錯，因為方法 PickObject() 回傳型別為reference
+            IList<Reference> references = sel.PickObjects(ObjectType.Element, "選擇要加入的管");
+
+
+            //IList<Element> elems = doc.GetElement(references);
+            //上面這行報錯，因為方法 GetElement() 回傳型別為reference(單數)
+
+
+            List<Element> elems = new List<Element>();
+            foreach (Reference refer in references)
+            {
+                Element elem = doc.GetElement(refer);
+                st.AppendLine("elem.Id：" + elem.Id);
+                elems.Add(elem);
+            }
+            //references並無點選排序
+            //上面這種作法雖然可行，但...?
+            st.AppendLine($"references數量為：{references.Count}");
+            st.AppendLine($"elems數量為：{elems.Count}");
+
+            
+            MessageBox.Show(st.ToString());
+            return Result.Succeeded;
+        }
+    */
+    #endregion
+
+    #region(執行外掛後點選取多個物件的方法_2) 
+    public class Test02_PickMultiObjects : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            Selection sel = uidoc.Selection;
+            StringBuilder st = new StringBuilder();
+
+            ISelectionFilter gagaFilter = new SelectElementsFilter(doc);
+            //List<Reference> selPipeRefs = sel.PickObjects(ObjectType.Element, gagaFilter, "選擇要加入的管").ToList();
+            IList<Reference> selPipeRefs = sel.PickObjects(ObjectType.Element, gagaFilter, "選擇要加入的管");
+            foreach (Reference refer in selPipeRefs)
+            {
+                Element e = doc.GetElement(refer);
+                st.AppendLine("element.Id：" + e.Id);
+            }
+
+
+            MessageBox.Show(st.ToString());
+            return Result.Succeeded;
+        }
+        public class SelectElementsFilter : ISelectionFilter
+        {
+            Document docDefault = null;
+            public SelectElementsFilter(Document doc)
+            {
+                docDefault = doc;
+            }
+
+            public bool AllowElement(Element eForFil)
+            {
+                if (eForFil.Category.Id.IntegerValue == BuiltInCategory.OST_PipeCurves.GetHashCode())
+                //if (eForFil.Category.Id == Category.GetCategory(docDefault, BuiltInCategory.OST_PipeCurves).Id)
+                //上面這行可以運作，但其實不用再透過文件去撈
+                    return true;
+                else
+                    return false;
+            }
+
+            public bool AllowReference(Reference r, XYZ p)
+            {
+                return true;
+            }
+        }
+
+
+
+    }
+    #endregion
+}
---
+++ Test_01_03/Test03_HighlightSelObjSort.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+
+
+namespace Test_01_03
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class Test03_HighlightSelObjSort : IExternalCommand
+    {
+		public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+		{
+			UIDocument uidoc = commandData.Application.ActiveUIDocument;
+			Document doc = uidoc.Document;
+			Selection selection = uidoc.Selection;
+			List<ElementId> selectId = new List<ElementId>();
+			//上面這行需比較List與Ilist的差異
+			StringBuilder st = new StringBuilder();
+
+			bool flag = true;
+			//TransactionManager.Instance.ForceCloseTransaction()
+			//just to make sure everything is closed down
+			//上面這行查到的是說：
+			//Dynamo's transaction handling is pretty poor for multiple documents,
+			//so we'll need to force close every single transaction we open
+
+			TaskDialog.Show("Selection", "Pick elements in the desired order (re-select to Remove), hit ESC to stop picking.");
+			TransactionGroup tg = new TransactionGroup(doc, "tgSelection");
+			tg.Start();
+			
+
+			while (flag == true)
+				try
+				{
+					Reference refer = selection.PickObject(ObjectType.Element, "Pick elements in the desired order (re-select to Remove), hit ESC to stop picking.");
+					ElementId e_id = refer.ElementId;
+					if (!selectId.Contains(e_id))
+					{
+						highlight hi = new highlight(doc, uidoc);
+						hi.overridecolor(e_id, true);
+						selectId.Add(e_id);
+					}
+					else
+					{
+						highlight hi = new highlight(doc, uidoc);
+						hi.overridecolor(e_id, false);
+                        selectId.RemoveAt(selectId.IndexOf(e_id));
+					}
+				}
+
+				catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+				{
+					flag = false;
+					break;
+				}
+
+			tg.RollBack();
+			foreach (ElementId xId in selectId)
+			{
+				st.AppendLine("element.Id：" + xId);
+			}
+
+			
+			MessageBox.Show(st.ToString());
+			return Result.Succeeded;
+
+		}
+		public class highlight 
+		{
+			Transaction t = null;
+			Document d = null;
+			Autodesk.Revit.DB.View view = null;
+			bool reset;
+			Color color_rgb = new Color(30, 144, 255);
+			OverrideGraphicSettings gSettings;
+			UIDocument uidocument;
+
+			public highlight(Document doc, UIDocument uidoc)
+			{
+				d = doc;
+				t = new Transaction(doc, "Selection");
+				view = d.ActiveView;
+				uidocument = uidoc;
+			}
+			public void overridecolor(ElementId elementId, bool resetting)
+			{
+				gSettings = new OverrideGraphicSettings();
+				t.Start();
+				reset = resetting;
+
+				
+				if (reset != false)
+				{
+					gSettings.SetSurfaceBackgroundPatternColor(color_rgb);
+					gSettings.SetSurfaceForegroundPatternColor(color_rgb);
+					gSettings.SetProjectionLineColor(color_rgb);
+					gSettings.SetCutLineColor(color_rgb);
+					gSettings.SetCutForegroundPatternColor(color_rgb);
+					gSettings.SetProjectionLineWeight(8);
+					gSettings.SetSurfaceTransparency(80);
+				}
+				
+				view.SetElementOverrides(elementId, gSettings);
+				d.Regenerate();
+				uidocument.RefreshActiveView();
+				t.Commit();
+				t.Dispose();
+			}
+		}
+
+
+							
+        
+    }
+}
